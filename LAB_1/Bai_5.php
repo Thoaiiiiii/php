68d51b0889191a68435168ae6bf4e019b5be81a2
Thoaiiiiii <quocthoai2306@gmail.com>
2025-03-15 14:15:37 +0700

    Update Chuan Hoa String

--- LAB_1/Bai_5.php
+++ LAB_1/Bai_5.php
@@ -1,15 +1,21 @@
-<?php
-    $array = ["Nguyen Van An", "Tran Thi Ngoc!", "Tran @Thi Lan", "Nguyen2 Thi Hoa1238", "#Bui Anh Tuan"];
-    function locChuoi($array){
-        return array_filter($array, fn($string) => preg_match('/[^a-zA-Z0-9\s]/', $string));
+<?php 
+    $string_array = ["Nguyen Van An", "Tran Thi Ngoc!", "Tran @Thi Lan", "Nguyen2 Thi Hoa1238", "#Bui Anh Tuan"];
+    function locChuoiChuaKyTuDacBiet($string_array){
+        $res = [];
+        foreach($string_array as $string){
+            if(preg_match("/[^a-zA-Z\s]/", $string)){
+                $stringChuanHoa = chuanHoaChuoi($string);
+                $res[] = $stringChuanHoa;
+            }
+        }
+        return $res;
     }
 
-    function dinhDang($string){
+    function chuanHoaChuoi($string){
+        $string = preg_replace("/[^a-zA-Z\s]/", "", $string);
+        
         return ucwords(strtolower($string));
     }
-
-    $mangloc = locChuoi($array);
-    $res = array_map('dinhDang', $mangloc);
-    echo "Cac chuoi sau khi loc va dinh dang la: \n";
+    $res = locChuoiChuaKyTuDacBiet($string_array);  
     print_r($res);
 ?>
